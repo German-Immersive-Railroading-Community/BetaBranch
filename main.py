91c3f17e57609436ff8971dc2854a3a6d9e595c3
Raziel Kretschmer <kretschmer.raziel@gmail.com>
2021-07-23 20:17:25 +0200

    Third Test

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 from http.server import HTTPServer, BaseHTTPRequestHandler
 import ssl
 from decouple import config
+import json
 
 class Requests(BaseHTTPRequestHandler):
-    pass
+    def do_POST(self):
+        print(self.rfile.read())
 
 context = ssl.SSLContext(ssl.PROTOCOL_TLS_SERVER)
 context.load_cert_chain(config("cert_path"), config("priv_path"))
@@ -11,3 +13,4 @@ context.load_verify_locations(config("full_path"))
 
 httpd = HTTPServer(('0.0.0.0', 4433), Requests)
 httpd.socket = context.wrap_socket(httpd.socket, server_side=True)
+httpd.serve_forever()
