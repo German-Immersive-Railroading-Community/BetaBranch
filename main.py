a01a8c42a77e71d2c0aaf5b5de7a36fc926382fe
Raziel Kretschmer <kretschmer.raziel@gmail.com>
2021-07-24 13:12:49 +0200

    Send responses

--- main.py
+++ main.py
@@ -1,17 +1,23 @@
-from http.server import HTTPServer, BaseHTTPRequestHandler
-import ssl
-from decouple import config
-import json
 import hashlib as hl
 import hmac
+import json
+import ssl
+from http.server import BaseHTTPRequestHandler, HTTPServer
+
+from decouple import config
+
 
 class Requests(BaseHTTPRequestHandler):
     def do_POST(self):
         _rfile = self.rfile.read()
         h_object = hmac.new(bytes(config("secret"), "utf8"), _rfile, hl.sha256)
-        h_digest = h_object.hexdigest()
-        print(h_digest)
-        print(self.headers)
+        h_digest = str(h_object.hexdigest())
+        h_digest = "sha256=" + h_digest
+        if not h_digest == str(self.headers["X-Hub-Signature-256"]):
+            self.send_error(403, "Forbidden")
+            return
+        self.send_response(200, "Sucessfull")
+
 
 context = ssl.SSLContext(ssl.PROTOCOL_TLS_SERVER)
 context.load_cert_chain(config("cert_path"), config("priv_path"))
